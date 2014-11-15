2f77fe3b434bbb2a7b5f2d7a9d9a68f72854d3f3
Scott <scottshuffler@appsrc2.cs.appstate.edu>
2014-11-15 00:43:49 -0500

    UPDATED

--- download.py
+++ download.py
@@ -4,26 +4,33 @@ import sys
 import cv2
 import os
 
-yt = YouTube()
 
-yt.url = sys.argv[1]
+# name = (str(yt.filename) + ".mp4")
+# print name
+# vidfile = cv2.VideoCapture(name)
+# cv2.namedWindow("Current frame")
+# ret, currFrame = vidfile.read()
 
-video = yt.get('mp4', '480p')
-video = yt.get('mp4')
-video.download()
-name = (str(yt.filename) + ".mp4")
-print name
-vidfile = cv2.VideoCapture(name)
-cv2.namedWindow("Current frame")
-ret, currFrame = vidfile.read()
+# while(ret):
+#     cv2.imshow("Current frame", currFrame)
+#     ret, currFrame = vidfile.read()
+#     key = cv2.waitKey(33)
+#     if key is 113: 
+#         break
 
-while(ret):
-    cv2.imshow("Current frame", currFrame)
-    ret, currFrame = vidfile.read()
-    key = cv2.waitKey(33)
-    if key is 113: 
-        break
-vidfile.release()
 
-print yt.filename
-os.remove(name)
+class Download(object):
+    def __init__(self,url):
+        self.url = url
+        yt = YouTube()
+        video = yt.get('mp4', '480p')
+        video = yt.get('mp4')
+        video.download()
+        self.name = (str(yt.filename) + ".mp4")
+
+    def getVideo(self):
+        self.vidfile = cv2.VideoCapture(self.name)
+
+    def __del__(self):
+        self.vidfile.release()
+        os.remove(self.name)
